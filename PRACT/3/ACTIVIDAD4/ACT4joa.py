1aa8535d19278490ccc82d2afd1078ffadbe7de8
JoaquinBaldevenito <joaquinbaldevenito@gmail.com>
2025-09-26 14:26:16 -0300

    B

--- PRACT/3/ACTIVIDAD4/ACT4joa.py
+++ PRACT/3/ACTIVIDAD4/ACT4joa.py
@@ -42,8 +42,9 @@ X = np.array([[0,0],
               [1,0],
               [2,2],
               [2,3],
-              [3,2]])
-y = np.array([0,0,0,1,1,1])
+              [3,2],
+              [3,3]])
+y = np.array([0,0,0,1,1,1,0])
 
 # Entrenamiento
 w, b, step = perceptron_train(X, y)
